f864daa8813563021d2d981fa1622db96a129ae0
Matheus <matheus.manucci@gmail.com>
2021-03-05 23:32:25 -0300

    annotate on models

--- app/models/freight.rb
+++ app/models/freight.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: freights
+#
+#  id         :bigint           not null, primary key
+#  state      :string
+#  weight_min :float
+#  weight_max :float
+#  cost       :float
+#
 class Freight < ApplicationRecord
   validates :state, :weight_min, :weight_max, :cost, presence: true
 end
--- app/models/power_generator.rb
+++ app/models/power_generator.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: power_generators
+#
+#  id             :bigint           not null, primary key
+#  name           :string
+#  description    :string
+#  image_url      :string
+#  manufacturer   :string
+#  structure_type :integer
+#  price          :float
+#  height         :float
+#  width          :float
+#  lenght         :float
+#  weight         :float
+#  kwp            :float
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class PowerGenerator < ApplicationRecord
+  paginates_per 6
   validates :name, :description, :image_url, :manufacturer, :price, :kwp, presence: true
   validates :height, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 40 }
   validates :width, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
